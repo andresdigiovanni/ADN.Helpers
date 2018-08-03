f89425f22c6cf6e82ceac51cf1728a2067e3bdda
Andrés <andresdigiovanni@gmail.com>
2018-08-03 18:42:02 +0200

    DateTimeHelperTest with culture

--- tests/ADN.Helpers.Tests/src/Data/DateTimeHelperTest.cs
+++ tests/ADN.Helpers.Tests/src/Data/DateTimeHelperTest.cs
@@ -1,6 +1,7 @@
 ﻿using ADN.Helpers.Data;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using Xunit;
 
@@ -9,11 +10,13 @@ namespace ADN.Helpers.Tests.Data
     public class DateTimeHelperTest
     {
         [Theory]
-        [InlineData("01/01/1970 00:00:00", 0)]
-        [InlineData("16/02/2008 12:15:12", 1203164112)]
-        public void DateTimeToUnixTimeStamp(string value, long expected)
+        [InlineData("01/01/1970 00:00:00", "en-US", 0)]
+        [InlineData("01/01/1970 00:00:00", "es-ES", 0)]
+        [InlineData("02/16/2008 12:15:12", "en-US", 1203164112)]
+        [InlineData("16/02/2008 12:15:12", "es-ES", 1203164112)]
+        public void DateTimeToUnixTimeStamp(string value, string culture, long expected)
         {
-            DateTime date = DateTime.Parse(value);
+            DateTime date = DateTime.Parse(value, new CultureInfo(culture));
             var result = DateTimeHelper.DateTimeToUnixTimeStamp(date);
 
             Assert.Equal(expected, result);
@@ -21,10 +24,10 @@ namespace ADN.Helpers.Tests.Data
 
         [Theory]
         [InlineData(0, "01/01/1970 00:00:00")]
-        [InlineData(1203164112, "16/02/2008 12:15:12")]
+        [InlineData(1203164112, "02/16/2008 12:15:12")]
         public void UnixTimeStampToDateTime(long value, string expected)
         {
-            var result = DateTimeHelper.UnixTimeStampToDateTime(value).ToString("dd/MM/yyyy HH:mm:ss");
+            var result = DateTimeHelper.UnixTimeStampToDateTime(value).ToString("MM/dd/yyyy HH:mm:ss");
 
             Assert.Equal(expected, result);
         }
