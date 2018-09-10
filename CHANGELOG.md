953d85815599457629bbedf5405e5e90ed332a85
Andrés <andresdigiovanni@gmail.com>
2018-09-10 11:37:11 +0200

    Added FileHelper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 ### Added
-- Helper class: DirectoryHelper.
+- Helpers classes: DirectoryHelper, FileHelper.
 
 ## [1.0.0] - 2018-08-04
 ### Added
---
+++ src/ADN.Helpers/src/IO/FileHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ADN.Helpers.IO
+{
+    /// <summary>
+    /// A static class of extension methods for <see cref="File"/>.
+    /// </summary>
+    public class FileHelper
+    {
+        /// <summary>
+        /// Check if a file is open.
+        /// </summary>
+        /// <param name="file">File name.</param>
+        /// <param name="fileAccess">File access.</param>
+        public static bool IsFileOpen(string file, FileAccess fileAccess = FileAccess.ReadWrite)
+        {
+            try
+            {
+                FileStream fs = new FileStream(file, FileMode.Open, fileAccess);
+                fs.Close();
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
