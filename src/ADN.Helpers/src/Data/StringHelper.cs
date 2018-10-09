3037db2eed7c25a9eee55a327259b6a76f0d1ae8
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-10-09 11:00:59 +0200

    Added StringHelper

---
+++ src/ADN.Helpers/src/Data/StringHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADN.Helpers.Data
+{
+    /// <summary>
+    /// A static class of extension methods for <see cref="string"/>.
+    /// </summary>
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Returns a string containing a specified number of characters from the left side of a string.
+        /// </summary>
+        /// <param name="str">String expression from which the leftmost characters are returned.</param>
+        /// <param name="length">Numeric expression indicating how many characters to return.</param>
+        /// <returns>Returns a string containing a specified number of characters from the left side of a string.</returns>
+        public static string Left(string str, int length)
+        {
+            return (str.Length <= length) ? str : str.Substring(0, length);
+        }
+
+        /// <summary>
+        /// Returns a string that contains a specified number of characters starting from a specified position in a string.
+        /// </summary>
+        /// <param name="str">String expression from which characters are returned.</param>
+        /// <param name="startIndex">Starting position of the characters to return.</param>
+        /// <param name="length">Number of characters to return.</param>
+        /// <returns>A string that consists of the specified number of characters starting from the specified position in the string.</returns>
+        public static string Mid(string str, int startIndex, int length)
+        {
+            if (str.Length < startIndex) return "";
+
+            return (str.Length < startIndex + length) ? str.Substring(startIndex, str.Length - startIndex)
+                                                      : str.Substring(startIndex, length);
+        }
+
+        /// <summary>
+        /// Returns a string containing a specified number of characters from the right side of a string.
+        /// </summary>
+        /// <param name="str">String expression from which the rightmost characters are returned.</param>
+        /// <param name="length">Numeric expression indicating how many characters to return.</param>
+        /// <returns>Returns a string containing a specified number of characters from the right side of a string.</returns>
+        public static string Right(string str, int length)
+        {
+            return (str.Length <= length) ? str : str.Substring(str.Length - length, length);
+        }
+    }
+}
---
+++ tests/ADN.Helpers.Tests/src/Data/StringHelperTest.cs
@@ -0,0 +1,46 @@
+﻿using ADN.Helpers.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ADN.Helpers.Tests.Data
+{
+    public class StringHelperTest
+    {
+        [Theory]
+        [InlineData("abcdefghij", 0, "")]
+        [InlineData("abcdefghij", 5, "abcde")]
+        [InlineData("abcdefghij", 10, "abcdefghij")]
+        [InlineData("abcdefghij", 15, "abcdefghij")]
+        public void Left(string value, int length, string expected)
+        {
+            var result = StringHelper.Left(value, length);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("abcdefghij", 0, 0, "")]
+        [InlineData("abcdefghij", 0, 5, "abcde")]
+        [InlineData("abcdefghij", 5, 10, "fghij")]
+        [InlineData("abcdefghij", 5, 15, "fghij")]
+        public void Mid(string value, int startIndex, int length, string expected)
+        {
+            var result = StringHelper.Mid(value, startIndex, length);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("abcdefghij", 0, "")]
+        [InlineData("abcdefghij", 5, "fghij")]
+        [InlineData("abcdefghij", 10, "abcdefghij")]
+        [InlineData("abcdefghij", 15, "abcdefghij")]
+        public void Right(string value, int length, string expected)
+        {
+            var result = StringHelper.Right(value, length);
+            Assert.Equal(expected, result);
+        }
+    }
+}
