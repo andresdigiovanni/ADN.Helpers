35a9f94a5179d659c869a8aadc23e0faa9c0318e
Andrés Di Giovanni <agiovanni@jbctools.com>
2018-11-20 13:59:23 +0100

    Added EnumHelper

---
+++ src/ADN.Helpers/Models/Data/EnumHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADN.Helpers.Data
+{
+    /// <summary>
+    /// A static class of extension methods for <see cref="Enum"/>.
+    /// </summary>
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Get enum value.
+        /// </summary>
+        /// <param name="enumType">Enum type.</param>
+        /// <param name="name">Enum text value.</param>
+        /// <param name="ignoreCase">Ignore case sensitive.</param>
+        /// <returns>Enum value.</returns>
+        public static Enum GetEnumValue(Type enumType, string name, bool ignoreCase = false)
+        {
+            int index;
+            if (ignoreCase)
+            {
+                index = Array.FindIndex(
+                    Enum.GetNames(enumType),
+                    s => string.Compare(s, name, StringComparison.OrdinalIgnoreCase) == 0);
+            }
+            else
+            {
+                index = Array.IndexOf(Enum.GetNames(enumType), name);
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentException("\"" + name + "\" is not a value in " + enumType, "name");
+            }
+
+            return (Enum)Enum.GetValues(enumType).GetValue(index);
+        }
+    }
+}
---
+++ tests/ADN.Helpers.Tests/Models/Data/EnumHelperTest.cs
@@ -0,0 +1,37 @@
+﻿using ADN.Helpers.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADN.Helpers.Tests.Data
+{
+    public class EnumHelperTest
+    {
+        public enum Foo
+        {
+            A = 1,
+            B = 2,
+            C = 4,
+            D = 8
+        }
+
+        [Theory]
+        [InlineData("A", Foo.A, false)]
+        [InlineData("a", Foo.A, true)]
+        [InlineData("B", Foo.B, false)]
+        [InlineData("b", Foo.B, true)]
+        public void GetEnumValue(string value, Foo expected, bool ignoreCase)
+        {
+            var result = EnumHelper.GetEnumValue(expected.GetType(), value, ignoreCase);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetEnumValue_Exception_Argument()
+        {
+            Assert.Throws<ArgumentException>(() => EnumHelper.GetEnumValue(Foo.A.GetType(), "Z"));
+        }
+    }
+}
